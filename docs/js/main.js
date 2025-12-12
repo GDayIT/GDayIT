b14e30aa133c36d76a2dd1907e42c65366ed5279
Dimitrios Georgiou <d.georgiou@edu.bbq.de>
2025-12-12 03:09:24 +0100

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,12 @@
+<script>
+document.querySelector('a[href="#howido"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.getElementById("howido-modal").style.display = "flex";
+  document.getElementById("howido-video").play();
+});
 
+document.querySelector(".close-btn").addEventListener("click", function() {
+  document.getElementById("howido-modal").style.display = "none";
+  document.getElementById("howido-video").pause();
+});
+</script>
